626cf2981e50217a9e65873ad7bf6fe28743975e
github username <github email address>
2020-10-23 12:30:21 -0400

    continues to work on initial fetch

--- tasks-frontend/index.js
+++ tasks-frontend/index.js
@@ -3,31 +3,44 @@ const BASE_URL = 'http://localhost:3000'
 const morningRoutine = document.getElementById('morning-routine')
 const homework = document.getElementById('homework')
 const chores = document.getElementById('chores')
-const bedTime = document.getElementById('bedtime')
+const bedtime = document.getElementById('bedtime')
 const newTaskForm = document.getElementById('new-task-form')
 const taskFormDiv = document.getElementById('task-form')
+const morningUl = morningRoutine.querySelector('ul')
+const homeworkUl = homework.querySelector('ul')
+const choresUl = chores.querySelector('ul')
+const bedtimeUl = bedtime.querySelector('ul')
 
 //  ***************************** start up routine **************************************************
 document.addEventListener("DOMContentLoaded", () =>{
-    getMorningTasks()
-    getHomeworkTasks()
-    getChoresTasks()
-    getBedtimeTasks()
+    getAllTasks()
+    // getHomeworkTasks()
+    // getChores()
+    // getBedtimeTasks()
 });
 
 
 //  **************************** requests to backend ************************************************
-function getMorningTasks() {
+function getAllTasks() {
     fetch(BASE_URL + '/tasks')
     .then(response => response.json())
     .then(tasks => {
-       let morningTask = tasks.find(task => task.routine === "Morning")
-        morningRoutine.innerHTML += tasks.map(task => console.log(task))
-
-       
+        if (tasks.routine === "Morning") {
+            morningUl.innerHTML = makeTaskList(task)
+        }
+     debugger
     })
 }
 
 
 
 //  ********** helpers for generating HTML and adding event listeners *******************************
+
+function makeTaskList(task) {
+    debugger
+    return (
+        `<li>
+            <a href="" data-id="${task.id}">${task.name}</a> - ${todo.completed ? "Completed" : "Not Completed"}
+        </li>`
+    )
+};
