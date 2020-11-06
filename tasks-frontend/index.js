8d1e06f88c31d7e1ed515eabfa26c8472f39fcc4
github username <github email address>
2020-11-06 13:52:36 -0500

    refactors for OO classes

--- tasks-frontend/index.js
+++ tasks-frontend/index.js
@@ -31,17 +31,21 @@ function getAllTasks() {
         homeworkUl.innerHTML = ''
         choresUl.innerHTML = ''
         bedtimeUl.innerHTML = ''
-        allTasks.forEach(function(task) {
-           if (task.routine === "Morning") {
-            morningUl.innerHTML += makeTaskList(task)
+        miscUl.innerHTML = ''
+        allTasks.forEach(task => {
+            
+            const taskInstance = new Task(task)
+           
+            if (task.routine === "Morning") {
+            morningUl.innerHTML += taskInstance.renderTask()
            } else if (task.routine === "Homework") {
-               homeworkUl.innerHTML += makeTaskList(task)
+               homeworkUl.innerHTML += taskInstance.renderTask()
            } else if (task.routine === "Chore") {
-               choresUl.innerHTML += makeTaskList(task)
+               choresUl.innerHTML += taskInstance.renderTask()
            } else if (task.routine === "Bedtime") {
-               bedtimeUl.innerHTML += makeTaskList(task)
+               bedtimeUl.innerHTML += taskInstance.renderTask()
            } else if (task.routine === "") {
-               miscUl.innerHTML += makeTaskList(task)
+               miscUl.innerHTML += taskInstance.renderTask()
            }
         });
         clickOnTasks()
@@ -107,21 +111,24 @@ function createTask() {
     fetch(BASE_URL + "/tasks", configObj)
     .then(response => response.json())
     .then(task => {
+
+        const taskInstance = new Task(task)
+
         if (task.routine === "Morning") {
             taskFormDiv.innerHTML = ''
-            morningUl.innerHTML += makeTaskList(task)
+            morningUl.innerHTML += taskInstance.renderTask()
            } else if (task.routine === "Homework") {
                taskFormDiv.innerHTML = ''
-               homeworkUl.innerHTML += makeTaskList(task)
+               homeworkUl.innerHTML += taskInstance.renderTask()
            } else if (task.routine === "Chore") {
                taskFormDiv.innerHTML = ''
-               choresUl.innerHTML += makeTaskList(task)
+               choresUl.innerHTML += taskInstance.renderTask()
            } else if (task.routine === "Bedtime") {
                taskFormDiv.innerHTML = ''
-               bedtimeUl.innerHTML += makeTaskList(task)
+               bedtimeUl.innerHTML += taskInstance.renderTask()
            } else if (task.routine === "") {
                taskFormDiv.innerHTML = ""
-               miscUl.innerHTML += makeTaskList(task) 
+               miscUl.innerHTML += taskInstance.renderTask() 
            }
     })
 }
@@ -146,7 +153,10 @@ function editTask() {
     fetch(BASE_URL + `/tasks/${taskId}`)
     .then(response => response.json())
     .then(task => {
-        const editForm = editFormInputs(task)
+
+        const taskInstance = new Task(task)
+
+        const editForm = taskInstance.renderEditForm()
         taskFormDiv.innerHTML = editForm
         document.querySelector("form").addEventListener('submit', updateTask)
     })
@@ -182,46 +192,40 @@ function updateTask() {
     fetch(BASE_URL + `/tasks/${taskId}`, configObj)
     .then(response => response.json())
     .then(task => {
-        const updatedTask = 
-        `<li>
-        <a href="" data-id="${task.id}">${task.name}</a> - ${task.completed ? "Completed" : "Not Completed"}
-        <button id="delete" data-id="${task.id}">Delete</button>
-        <button id="update-task" data-id="${task.id}">Update</button>
-        </li>`
+        // const updatedTask = 
+        // `<li>
+        // <a href="" data-id="${task.id}">${task.name}</a> - ${task.completed ? "Completed" : "Not Completed"}
+        // <button id="delete" data-id="${task.id}">Delete</button>
+        // <button id="update-task" data-id="${task.id}">Update</button>
+        // </li>`
+
+        const taskInstance = new Task(task)
 
         if (task.routine === "Morning") {
             taskFormDiv.innerHTML = ''
-            morningUl.innerHTML = updatedTask
+            morningUl.innerHTML += taskInstance.renderUpdatedTask()
            } else if (task.routine === "Homework") {
                taskFormDiv.innerHTML = ''
-               homeworkUl.innerHTML = updatedTask
+               homeworkUl.innerHTML += taskInstance.renderUpdatedTask()
            } else if (task.routine === "Chore") {
                taskFormDiv.innerHTML = ''
-               choresUl.innerHTML = updatedTask
+               choresUl.innerHTML += taskInstance.renderUpdatedTask()
            } else if (task.routine === "Bedtime") {
                taskFormDiv.innerHTML = ''
-               bedtimeUl.innerHTML = updatedTask
+               bedtimeUl.innerHTML += taskInstance.renderUpdatedTask()
            } else if (task.routine === "") {
                taskFormDiv.innerHTML = ''
-               miscUl.innerHTML = updatedTask
-           }
+               miscUl.innerHTML += taskInstance.renderUpdatedTask()
+           } else if (task.routine === "") {
+               taskFormDiv.innerHTML = ""
+               miscUl.innerHTML += taskInstance.renderUpdatedTask() 
+        }
         
         clickOnTasks()
-       
     })
 }
 
 //  ********** helpers for generating HTML and adding event listeners *******************************
-
-function makeTaskList(task) {
-    return (
-        `<li>
-            <a href="" data-id="${task.id}">${task.name}</a> - ${task.completed ? "Completed" : "Not Completed"}
-            <button id="delete" data-id="${task.id}">Delete</button>
-            <button id="update-task" data-id="${task.id}">Update</button>
-        </li>`
-    )
-};
 
 function taskForm() {
     return (`
@@ -310,42 +314,6 @@ function showSingleTask(task) {
     }
 }
 
-
-
-function editFormInputs(task) {
-    const string1 = `
-    <form data-id=${task.id}>
-      <label for="name">Task:</label>      
-      <input type="text" id="task-name" value="${task.name}">
-      <label for="completed">Click Box If Task Is Not Complete:</label>
-      <input type="checkbox" id="completed" ${task.completed ? "checked" : ""}><br>`
-
-    let string2 = ``
-    let instructions = task.instructions
-    for(let i = 0; i < instructions.length; i++) {
-       string2 += `<label for="instruction">Instruction</label>
-                   <input type="text" id="${instructions[i].id}" class="edit-task-instructions" value="${instructions[i].description}"><br>`
-    }
-    
-    const string3 = `
-    <label for="routine">Routine:</label>
-    <select name="routine" id="edit-task-routine">
-       <option disable selected value>-- Select A Routine --</option>
-       <option value="Morning">Morning</option>
-       <option value="Homework">Homework</option>
-       <option value="Chore">Chores</option>
-       <option value="Bedtime">Bedtime</option>
-    </select><br>
-    <input type="submit">
-    </form>`
-
-    
-
-    return string1 + string2 + string3
-}
-
-
-
 function clickOnTasks() {
     const liLinks = document.querySelectorAll("li a")
 
@@ -375,3 +343,65 @@ function clickOnTasks() {
         
 //     }
 // }
+
+
+/////////////////////////////////////// CLASSES ////////////////////////////////////////////////////
+
+class Task {
+    constructor(task) { //object from json
+        this.id = task.id
+        this.name = task.name
+        this.routine = task.routine
+        this.completed = task.completed
+        this.instructions = task.instructions
+    };
+
+    renderTask() {
+        console.log('this is', this)
+        return (`
+            <li>
+              <a href="" data-id="${this.id}">${this.name}</a> - ${this.completed ? "Completed" : "Not Completed"}
+              <button id="delete" data-id="${this.id}">Delete</button>
+              <button id="update-task" data-id="${this.id}">Update</button>
+            </li>`
+        )
+    }
+
+    renderEditForm() {
+        const editForm1 = `
+    <form data-id=${this.id}>
+      <label for="name">Task:</label>      
+      <input type="text" id="task-name" value="${this.name}">
+      <label for="completed">Click Box If Task Is Not Complete:</label>
+      <input type="checkbox" id="completed" ${this.completed ? "checked" : ""}><br>`
+
+    let editForm2 = ``
+    let instructions = this.instructions
+    for(let i = 0; i < instructions.length; i++) {
+       editForm2 += `<label for="instruction">Instruction</label>
+                   <input type="text" id="${instructions[i].id}" class="edit-task-instructions" value="${instructions[i].description}"><br>`
+    }
+    
+    const editForm3 = `
+    <label for="routine">Routine:</label>
+    <select name="routine" id="edit-task-routine">
+       <option disable selected value>-- Select A Routine --</option>
+       <option value="Morning">Morning</option>
+       <option value="Homework">Homework</option>
+       <option value="Chore">Chores</option>
+       <option value="Bedtime">Bedtime</option>
+    </select><br>
+    <input type="submit">
+    </form>`
+
+    return editForm1 + editForm2 + editForm3
+    }
+
+    renderUpdatedTask() {
+        `<li>
+        <a href="" data-id="${this.id}">${this.name}</a> - ${this.completed ? "Completed" : "Not Completed"}
+        <button id="delete" data-id="${this.id}">Delete</button>
+        <button id="update-task" data-id="${this.id}">Update</button>
+        </li>`
+    }
+}
